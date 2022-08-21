ea5739167d7a24a9190b1fb0d200bde7fc5da273
Debora Faria <deeborafaria@gmail.com>
2022-08-21 15:33:11 -0300

    update

--- Engenharia-III/Strategy/antipattern/Cliente.java
+++ Engenharia-III/Strategy/antipattern/Cliente.java
@@ -1,10 +1,8 @@
 package antipattern;
 
 //Comportamento de emprestar livros da biblioteca: Estudantes e Professores tem condicoes especiais
-
 //O problema eh que o emprestimo sem condicoes especiais esta sendo
 //duplicado nas classes Estudante e Outros
-
 //Conclusao: Heranca eh um antipattern
 
 public abstract class Cliente {
