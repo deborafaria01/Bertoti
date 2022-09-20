bbf13e6831031b0d3139517c0daf4fe38090fbaf
Debora Faria <deeborafaria@gmail.com>
2022-09-20 11:39:09 -0300

    Update

---
+++ Engenharia-III/Observer/pattern/RevistaInformatica.java
@@ -0,0 +1,28 @@
+import java.util.Observable;
+
+public class RevistaInformatica extends Observable {
+
+	private int edicao;
+
+	public void setNovaEdicao(int novaEdicao) {
+		this.edicao = novaEdicao;
+
+		setChanged();
+		notifyObservers();
+	}
+
+	public int getEdicao() {
+		return this.edicao;
+	}
+
+	public static void main(String[] args) {
+		//poderia receber a nova edicao atraves de um recurso externo
+		int novaEdicao = 3;
+		RevistaInformatica revistaInformatica = new RevistaInformatica();
+		Assinante1 Assinante1 = new Assinante1(revistaInformatica);
+
+		revistaInformatica.setNovaEdicao(novaEdicao);
+	}
+
+}
+
